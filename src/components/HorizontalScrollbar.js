adb10f737a9f61b0b2ce8dcb134c49f68d48150f
Lidia Setubal <lidiasetubalbarbosa@hotmail.com>
2022-06-24 19:54:20 -0300

    Fetch Data

---
+++ src/components/HorizontalScrollbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Box } from '@mui/material';
+
+const HorizontalScrollbar = ({ data }) => {
+  return (
+    <div>
+      {data.map((item) => <Box>
+
+      </Box>)}
+    </div>
+  )
+}
+
+export default HorizontalScrollbar
