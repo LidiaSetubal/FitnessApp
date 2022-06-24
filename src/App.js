a843e2e49267be0b808cae67ccfe1d6dbd39e3af
Lidia Setubal <lidiasetubalbarbosa@hotmail.com>
2022-06-24 16:08:48 -0300

    Hero Banner

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Router, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import { Box } from '@mui/material';
 
 import './App.css';
