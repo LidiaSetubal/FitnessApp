a7d00eaade2bc02a74690429dd9c533bea87b340
Lidia Setubal <lidiasetubalbarbosa@hotmail.com>
2022-06-24 14:27:02 -0300

    reorganize folders

---
+++ src/components/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Stack } from '@mui/material';
+
+import Logo from '../assets/images/Logo.png';
+
+
+const Navbar = () => {
+  return (
+    <Stack>
+      <Link to="/">
+      <img src={Logo} alt="logo" style={{ width: '48px', height: '48px', margin: '0 20px'}}/>
+      </Link>
+    </Stack>
+  )
+}
+
+export default Navbar
