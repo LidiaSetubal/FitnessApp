28827c23fa6b629000d7fc64399edefa27107354
Lidia Setubal <lidiasetubalbarbosa@hotmail.com>
2022-06-26 19:06:13 -0300

    Body Part Component 2

--- src/components/BodyPart.js
+++ src/components/BodyPart.js
@@ -19,8 +19,13 @@ const BodyPart = ({ item, setBodyPart, bodyPart}) => {
       cursor: 'pointer',
       gap: '47px'
     }}
+    onClick={() => {
+      setBodyPart(item);
+    }}
     >
-      <img src={Icon} alt="dumbbell" styles={{ width: '40px', height: '40px'}} />
+      <img src={Icon} alt="dumbbell" style={{ width: '40px', height: '40px'}} />
+      <Typography fontSize="24px" fontWeight="bold"
+       color="#3A1212" textTransform="capitalize">{item}</Typography>
     </Stack>
   )
 }
