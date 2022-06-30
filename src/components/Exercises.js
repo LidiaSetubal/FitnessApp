d9707cd51fb3e88962215ccae39414c0bf7104ac
Lidia Setubal <lidiasetubalbarbosa@hotmail.com>
2022-06-29 23:07:13 -0300

    import exercisecard

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -3,6 +3,7 @@ import Pagination from '@mui/material/Pagination';
 import { Box, Stack, Typography } from '@mui/material/';
 
 import { exerciseOptions, fetchData } from '../utils/fetchData';
+import ExerciseCard from './ExerciseCard';
 
 const Exercises = ({exercises, setExercises, bodyPart}) => {
   console.log(exercises);
