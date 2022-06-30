6df96df8404797573c2b122e3c1ae6d5a8a3021f
Lidia Setubal <lidiasetubalbarbosa@hotmail.com>
2022-06-29 23:19:53 -0300

    adding Link and Stack in ExerciseCard

--- src/components/ExerciseCard.js
+++ src/components/ExerciseCard.js
@@ -1,8 +1,14 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Button, Stack, Typography } from '@mui/material';
 
-const ExerciseCard = () => {
+const ExerciseCard = ({ exercise }) => {
   return (
-    <div>ExerciseCard</div>
+    <Link className="exercise-card" to={`/exercise/${exercise.id}`}>
+      <img src={exercise.gifUrl} alt={exercise.name} loading="lazy" />
+      <Stack direction="row">
+      </Stack>
+    </Link>
   )
 }
 
