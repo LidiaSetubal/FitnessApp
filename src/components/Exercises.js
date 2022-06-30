b49eb512910dde2e040e2b821bc4240d90616cba
Lidia Setubal <lidiasetubalbarbosa@hotmail.com>
2022-06-29 23:09:41 -0300

    adding key

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -19,7 +19,7 @@ const Exercises = ({exercises, setExercises, bodyPart}) => {
       <Stack direction="row" sx={{ gap: { lg: '110px', xs: '50px'}}}
       flexWrap="wrap" justifyContent="center">
         {exercises.map((exercise, index) =>(
-          <p>{exercise.name}</p>
+          <ExerciseCard key={index} exercise={exercise}/>
         ))}
 
       </Stack>
