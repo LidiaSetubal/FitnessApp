69c24b50793408f62e062e4e7c4a3f03894ebc9b
Lidia Setubal <lidiasetubalbarbosa@hotmail.com>
2022-07-01 18:05:18 -0300

    ExerciseVideos import

--- src/components/ExerciseVideos.js
+++ src/components/ExerciseVideos.js
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from 'react';
+import { Typography, Box, Stack } from '@mui/material';
+import Loader from './Loader';
 
-const ExerciseVideos = () => {
-  return (
-    <div>ExerciseVideos</div>
-  )
-}
+const ExerciseVideos = ({ exerciseVideos, name }) => {
+  if (!exerciseVideos.length) return <Loader />;
 
-export default ExerciseVideos
+
+export default ExerciseVideos;
