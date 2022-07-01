7693140a542b3777f98d08f60cb8bbe25c925c8d
Lidia Setubal <lidiasetubalbarbosa@hotmail.com>
2022-07-01 17:32:06 -0300

    changes @mui/material

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Typography, Stack, Button } from '@mui/icons-material';
+import { Typography, Stack, Button } from '@mui/material';
 
 import BodyPartImage from '../assets/icons/body-part.png';
 import TargetImage from '../assets/icons/target.png';
