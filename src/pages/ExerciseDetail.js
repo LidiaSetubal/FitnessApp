e419176b55b1a26b88f80eb0d557d5826ec815e8
Lidia Setubal <lidiasetubalbarbosa@hotmail.com>
2022-06-30 23:05:26 -0300

    import Detail, ExerciseVideos, SimilarExercises

--- src/pages/ExerciseDetail.js
+++ src/pages/ExerciseDetail.js
@@ -3,6 +3,9 @@ import { useParams } from 'react-router-dom';
 import { Box } from '@mui/material';
 
 import { exerciseOptions, fetchData } from '../utils/fetchData';
+import Detail from '../components/Detail';
+import ExerciseVideos from '../components/ExerciseVideos';
+import SimilarExercises from '../components/SimilarExercises';
 
 const ExerciseDetail = () => {
   return (
