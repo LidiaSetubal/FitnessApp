e8bdd3456f272dbc73d1b382dd1492cc3a1a44d9
Lidia Setubal <lidiasetubalbarbosa@hotmail.com>
2022-07-01 18:02:32 -0300

    fetchdata final

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -5,10 +5,17 @@ export const exerciseOptions = {
     'X-RapidAPI-Key': process.env.REACT_APP_RAPID_KEY
   }
 };
+export const youtubeOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+    'X-RapidAPI-Key': 'f0021db587msh781fb1cbef39856p11c183jsn45521d5d1c85',
+  },
+};
 
 export const fetchData = async (url, options) => {
-  const response = await fetch(url, options);
-  const data = await response.json();
+  const res = await fetch(url, options);
+  const data = await res.json();
 
   return data;
-}
+};
