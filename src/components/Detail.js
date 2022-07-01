d52497c297067700c68fc69dddeb3ac7a40091cc
Lidia Setubal <lidiasetubalbarbosa@hotmail.com>
2022-07-01 17:30:10 -0300

    detail component 1

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import { Typography, Stack, Button } from '@mui/icons-material';
 
-const Detail = () => {
+import BodyPartImage from '../assets/icons/body-part.png';
+import TargetImage from '../assets/icons/target.png';
+import EquipmentImage from '../assets/icons/equipment.png';
+
+const Detail = ({exerciseDetail}) => {
+  const { bodyPart, gifUrl, name, target, equipment } = exerciseDetail;
+
   return (
-    <div>Detail</div>
+    <Stack gap="60px" sx={{flexDirection: { lg: 'row' }, p: '20px', alignItems: 'center'}}>
+      <img src={gifUrl} alt={name} loading="lazy" className="detail-image" />
+    </Stack>
   )
 }
 
