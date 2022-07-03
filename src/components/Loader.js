9853b33bcd133823496ad6eff0c490e3e4b5ff57
Lidia Setubal <lidiasetubalbarbosa@hotmail.com>
2022-07-02 22:15:47 -0300

    InfinitySpin

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Stack } from '@mui/material';
-// import { InfinitySpin } from 'react-loader-spinner';
+import { InfinitySpin } from 'react-loader-spinner';
 
 const Loader = () => (
   <Stack direction="row" justifyContent="center" alignItems="center" width="100%">
