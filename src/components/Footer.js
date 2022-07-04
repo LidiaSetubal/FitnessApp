46f46f872b1f46cb69ee1bc311b0a03c62f97edd
Lidia Setubal <lidiasetubalbarbosa@hotmail.com>
2022-07-03 23:25:11 -0300

    create a box

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,8 @@ import { Box, Stack, Typography } from '@mui/material';
 import Logo from '../assets/images/Logo-1.png';
 
 const Footer = () => (
-
+  <Box mt="80px" bgcolor="#FFF3F4">
+     </Box>
 );
 
 export default Footer;
