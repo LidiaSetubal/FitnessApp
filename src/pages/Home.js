ae890fb5aef429a006121ff5119fbacc4b43d099
Lidia Setubal <lidiasetubalbarbosa@hotmail.com>
2022-07-04 22:57:50 -0300

    home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,28 +2,20 @@ import React, { useState } from 'react';
 import { Box } from '@mui/material';
 
 import Exercises from '../components/Exercises';
-import HeroBanner from '../components/HeroBanner';
 import SearchExercises from '../components/SearchExercises';
+import HeroBanner from '../components/HeroBanner';
 
 const Home = () => {
-  const [bodyPart, setBodyPart] = useState('all');
   const [exercises, setExercises] = useState([]);
+  const [bodyPart, setBodyPart] = useState('all');
 
   return (
     <Box>
       <HeroBanner />
-      <SearchExercises
-        setExercises={setExercises}
-        bodyPart={bodyPart}
-        setBodyPart={setBodyPart}
-      />
-      <Exercises
-        exercises={exercises}
-        setExercises={setExercises}
-        bodyPart={bodyPart}
-      />
+      <SearchExercises setExercises={setExercises} bodyPart={bodyPart} setBodyPart={setBodyPart} />
+      <Exercises setExercises={setExercises} exercises={exercises} bodyPart={bodyPart} />
     </Box>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
